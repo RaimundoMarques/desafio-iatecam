bf7d9157ac79fbef4b6b27e2f106ea2c10536240
raimundo.marques <junnyhormarks@gmail.com>
2023-02-14 02:05:26 -0400

    methods CRUD

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request
-
 app = Flask(__name__)
 
 livros = [
@@ -20,13 +19,50 @@ livros = [
     },
 ]
 
-#Criar uma api para:
+# Criar uma api para:
 # - Consultar todos
-@app.route('/livros')
+
+
+@app.route('/livros', methods=['GET'])
 def obter_livros():
     return jsonify(livros)
 
+
 # - Consultar por id
-# - Editar 
-# - Ecluir
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livros_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+
+
+# - Editar
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+# - Criar
+
+
+@app.route('/livros', methods=['POST'])
+def criar_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(livros)
+
+# - Excluir
+
+
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+    return jsonify(livros)
+
+
 app.run(port=5000, host='localhost', debug=True)
