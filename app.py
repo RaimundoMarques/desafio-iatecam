000ba4ca639f2840e263d6a278dd04b0697be514
raimundo.marques <junnyhormarks@gmail.com>
2023-02-14 11:11:52 -0400

    fresh json

--- app.py
+++ app.py
@@ -17,6 +17,21 @@ livros = [
         'titulo': 'O senhor dos Anéis - O Retorno do Rei',
         'author': 'J.R.R Tolkien'
     },
+    {
+        'id': 4,
+        'titulo': 'O senhor dos Anéis - A Sociedade do Anel',
+        'author': 'J.R.R Tolkien'
+    },
+    {
+        'id': 5,
+        'titulo': 'O senhor dos Anéis - As Duas Torres',
+        'author': 'J.R.R Tolkien'
+    },
+    {
+        'id': 6,
+        'titulo': 'O senhor dos Anéis - O Retorno do Rei',
+        'author': 'J.R.R Tolkien'
+    }
 ]
 
 # Criar uma api para:
